73712ca161b3efd09427a2c5b0cb9edb96ab85b1
ferrles124 <mehmetemredursun9@gmail.com>
2026-06-09 14:51:40 +0300

    Request permissions in onCreate method

--- app/src/main/java/com/kendi/tarayicim/MainActivity.java
+++ app/src/main/java/com/kendi/tarayicim/MainActivity.java
@@ -59,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        PermissionHelper.requestIfNeeded(this);
+        
         dbHelper = new BrowserDatabaseHelper(this);
         adBlockEngine = new AdBlockEngine();
         tabManager = new TabManager(this);
